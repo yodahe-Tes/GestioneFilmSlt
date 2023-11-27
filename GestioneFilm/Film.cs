4e213637f2c67173f4cdbcdf96732274fe690f45
yodahe-Tes <yodaheteshome.kumbi@mail.polimi.it>
2023-11-27 18:10:43 +0100

    Initial interface and classes

---
+++ GestioneFilm/Film.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestioneFilm
+{
+    internal class Film : IMedia
+    {
+        public string? Titolo { get ; set ; }
+        public int Durata { get; set; }
+
+        enum Premio { Oscar , David , Grammy}
+        public void Play()
+        {
+            Random rnd = new Random();
+            int Durata = rnd.Next(1, 61);
+            Console.WriteLine("il film" + Titolo + "e al minuto " + Durata);
+            
+        }
+    }
+}
---
+++ GestioneFilm/IMedia.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestioneFilm
+{
+    internal interface IMedia
+    {
+        public string? Titolo { get; set; }
+        public int Durata { get; set; }
+
+        void Play();
+    }
+}
